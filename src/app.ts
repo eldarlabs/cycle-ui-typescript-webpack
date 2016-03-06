21a2fc28f272f6222e4acd7a358ba50a787ea8c1
arlair <arlair@yahoo.com>
2016-03-06 22:34:49 +0800

    add an input with no properties and some other headers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 //import Cycle from '@cycle/core';
 const Cycle = require('@cycle/core');
-const {div, h3, makeDOMDriver} = require('@cycle/dom');
+const {div, h1, h2, h3, makeDOMDriver} = require('@cycle/dom');
 const { Input, RadioButton } = require('@eldarlabs/cycle-ui');
 //TODO: how I want this to import
 // import { Input, RadioButton } from '@eldarlabs/cycle-ui';
@@ -8,9 +8,12 @@ import { Observable } from 'rx';
 
 function kitchenSinkView(sources) {
   return Observable.just(div([
-    h3('.example', [`Using Cycle-UI - Kitchen Sink`]),
+    h1('.example', [`Using Cycle-UI - Kitchen Sink`]),
+    h2([`Text Inputs`]),
+    h3([`Default Input (no properties)`]),
+    Input(sources).DOM,
     Input(sources, {
-        label: `Answer to life`,
+        label: `Input: Max Length`,
         maxLength: 50,
     }).DOM,
     RadioButton(sources, {
