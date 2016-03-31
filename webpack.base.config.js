7e0834cb13d76260bbf1c0d0d95f73de54238990
arlair <arlair@yahoo.com>
2016-03-31 23:11:23 +0800

    fix(build): attempt to fix compiling in source and tidy up webpack config

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -19,7 +19,6 @@ module.exports = new WebpackConfig().merge({
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './build'),
-    // publicPath: '/build/'
   },
 
   resolve: {
@@ -36,12 +35,8 @@ module.exports = new WebpackConfig().merge({
   module: {
     loaders: [
       { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.ts$/, loaders: ['ts-loader'],
-        include: [
-          path.join(__dirname, 'src'),
-        ],
-        exclude: /node_modules/,
-      }, {
+      { test: /\.ts$/, loader: 'ts-loader' },
+      {
         test: /\.(scss|css)$/,
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
       }
