721ddfa532ad79c0532998aa9bd2f1d4467aa441
arlair <arlair@yahoo.com>
2016-04-15 12:19:27 +0800

    fix(components): App component is no longer in react-toolbox, so taking it out and replacing it with

--- src/components/views/Main.ts
+++ src/components/views/Main.ts
@@ -1,18 +1,17 @@
-const { article } = require('cycle-snabbdom');
+const { article, div } = require('cycle-snabbdom');
 import { Observable as $ } from 'rx';
 import { HeaderView } from './Header';
 import { FooterView } from './Footer';
 import { KitchenSinkView } from '../../demo/kitchenSink/components/views/KitchenSink';
-import { App } from '@eldarlabs/cycle-ui';
 
 export function MainView$(sources: any) {
   return $.just(
-    App([
+    div([
       HeaderView(),
       article([
         KitchenSinkView(sources),
       ]),
       FooterView()
-    ]).DOM
+    ])
   );
 }
